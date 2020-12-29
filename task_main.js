1f28c8db93a8a9abac792a25cbfcef3324a36589
bobodeligong <48669265+bobodeligong@users.noreply.github.com>
2020-12-28 20:19:09 -0800

    Add files via upload

--- task_main.js
+++ task_main.js
@@ -1,57 +1,25 @@
 // define the site that hosts stimuli images
 var repo_site = "http:// bobodeligong.github.io/DemoContinuousReport/";
 
-// Instructions for Study Task
-var instructions_study = {
-	type : 'instructions',
-	pages: ['DEMO STUDY TASK </br></br> Each image will appear one by one. Study the color of each image. You will be asked to recall the colors later.</br></br>' +
-'Ready? </br></br>'],
-show_clickable_nav: true
-
-}
-
-// Instructions for Recall Task
-
-var instructions_test = {
-	type : 'instructions',
-	pages: ['DEMO RECALL TASK </br></br>Each image will appear one by one, in white. It will be surrounded by a black circle. ' +
-  'The circle acts like a color wheel--move your cursor, and the central image will change color. When the color matches your memory, ' +
-  'click the mouse to record your guess. Try to be as accurate as possible. </br></br>' +
-'Ready? </br></br>'],
-show_clickable_nav: true
-
-}
-
 // location of the images.
 var all_images = [repo_site + 'images/stim/1.svg',
 repo_site + 'images/stim/2.svg',
 repo_site + 'images/stim/3.svg',
 repo_site + 'images/stim/4.svg'];
 
-
 // Probably would want to randomize the stimuli, but for the demo, setting the stimulus order and the "correct" colors:
 var color_study_order = {
 
-  image: [1,2,3,4], // i.e. images/stim/1.svg, iamges/stim/2.svg, etc.
-  colIndex: [87,171,327,291], // these refer to the index of the "correct" color within colors.js
-};
-
-// Probably would want to randomize the stimuli, but for the demo, setting the stimulus order and the "correct" colors:
-var color_test_order = {
-
-  image: [1,2,3,4], // i.e. images/stim/1.svg, iamges/stim/2.svg, etc.
-  colIndex: [87,171,327,291], // these refer to the index of the "correct" color within colors.js
-};
-
-
-var fixationWhite = {
-	type: 'html-keyboard-response',
-	stimulus: '<div style="font-size:60px;">+</div>',
-	data: {test_part: 'fixation'},
-	choices: jsPsych.NO_KEYS,
-	trial_duration: 1000
-
-}
+    image: [1,2,3,4], // i.e. images/stim/1.svg, iamges/stim/2.svg, etc.
+    colIndex: [87,171,327,291], // these refer to the index of the "correct" color within colors.js
+  };
+  
+  // Probably would want to randomize the stimuli, but for the demo, setting the stimulus order and the "correct" colors:
+  var color_test_order = {
+  
+    image: [1,2,3,4], // i.e. images/stim/1.svg, iamges/stim/2.svg, etc.
+    colIndex: [87,171,327,291], // these refer to the index of the "correct" color within colors.js
+  };
 
 // study block stimuli
 var studyStim = [];
@@ -73,7 +41,32 @@ var testStim = [];
     };
 	};
 
-  var studyBlock = {
+/* create timeline */
+var timeline = [];
+
+// Instructions for Study Task
+var instructions_study = {
+	type : 'instructions',
+	pages: ['DEMO STUDY TASK </br></br> Each image will appear one by one. Study the color of each image. You will be asked to recall the colors later.</br></br>' +
+'Ready? </br></br>'],
+show_clickable_nav: true
+
+};
+timeline.push(instructions_study);
+
+/* study trials */
+var fixationWhite = {
+	type: 'html-keyboard-response',
+	stimulus: '<div style="font-size:60px;">+</div>',
+	data: {test_part: 'fixation'},
+	choices: jsPsych.NO_KEYS,
+	trial_duration: 1000
+
+}
+
+
+
+var studyBlock = {
       type: 'snap-keyboard-response',
       stimulus: jsPsych.timelineVariable('stimulus'),
       colIndex: jsPsych.timelineVariable('colIndex'),
@@ -84,6 +77,37 @@ var testStim = [];
         colIndex: jsPsych.timelineVariable('colIndex'),
       }
   };
+
+var study_procedure = {
+    timeline: [fixationWhite, studyBlock],
+    timeline_variables: studyStim
+
+}
+timeline.push(study_procedure);
+
+
+// Instructions for Recall Task
+
+var instructions_test = {
+	type : 'instructions',
+	pages: ['DEMO RECALL TASK </br></br>Each image will appear one by one, in white. It will be surrounded by a black circle. ' +
+  'The circle acts like a color wheel--move your cursor, and the central image will change color. When the color matches your memory, ' +
+  'click the mouse to record your guess. Try to be as accurate as possible. </br></br>' +
+'Ready? </br></br>'],
+show_clickable_nav: true
+
+}
+timeline.push(instructions_test);
+
+/* test trials */
+var fixationWhite = {
+	type: 'html-keyboard-response',
+	stimulus: '<div style="font-size:60px;">+</div>',
+	data: {test_part: 'fixation'},
+	choices: jsPsych.NO_KEYS,
+	trial_duration: 1000
+
+}
 
 var testBlock = {
     type: 'continuous_report',
@@ -96,16 +120,11 @@ var testBlock = {
     }
 };
 
-var study_procedure = {
-	    timeline: [fixationWhite, studyBlock],
-	    timeline_variables: studyStim
-
-}
 
 var test_procedure = {
 	    timeline: [fixationWhite, testBlock],
 	    timeline_variables: testStim
 
 }
-
-  var timeline = [instructions_study, study_procedure, instructions_test, test_procedure];
+timeline.push(test_procedure);
+  /* var timeline = [instructions_study, study_procedure, instructions_test, test_procedure]; */
