f9e9cd42fad2fbc968b34eea0850b2ec1b9c3f30
bobodeligong <48669265+bobodeligong@users.noreply.github.com>
2021-01-24 19:48:18 -0800

    Add files via upload

--- locations.js
+++ locations.js
@@ -1,8 +1,19 @@
+/*
 locations ={
     “locations”:[
         [centerXSVG-50, centerYSVG-150],
         [centerXSVG-150, centerYSVG-50],
         [centerXSVG-50, centerYSVG+50],
         [centerXSVG+50, centerYSVG-50]
+    ]
+};
+*/
+
+locations ={
+    “locations”:[
+        [250, 150],
+        [150, 250],
+        [250, 350],
+        [350, 250]
     ]
 };
