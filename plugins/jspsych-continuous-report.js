97421458a3cd22b7e2ee8de3030365ed74c88f65
bobodeligong <48669265+bobodeligong@users.noreply.github.com>
2021-01-31 01:30:48 -0800

    Add files via upload

--- plugins/jspsych-continuous-report.js
+++ plugins/jspsych-continuous-report.js
@@ -180,16 +180,17 @@ jsPsych.plugins["continuous_report"] = (function() {
     })
    */
 
+   // randomize probe locations based on svg paper dimensions
    var probLoc =[[centerXSVG-150, centerYSVG-50], [centerXSVG+50, centerYSVG-50]];
 
-   var probLocIndex = Math.round(Math.random());
+   // var probLocIndex = Math.round(Math.random());
 
     // % set the image position based on svg paper dimensions.
     // this may have to be changed depending on the size of the image. The demo images are 100 x 100.
     // var imageX = centerXSVG - 100 / 2-100;
     // var imageY = centerYSVG - 100 / 2;
-    var imageX = probLoc[probLocIndex][0];
-    var imageY = probLoc[probLocIndex][1];
+    var imageX = probLoc[trial.probLocIndex][0];
+    var imageY = probLoc[trial.probLocIndex][1];
 
     // load in the images
     var g = paper.group();
